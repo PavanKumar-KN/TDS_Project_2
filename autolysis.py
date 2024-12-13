38abd93e2940ab5bc51c057300b2f0181795c50f
PavanKumar-KN <21f1004929@ds.study.iitm.ac.in>
2024-12-13 09:34:35 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,3 +1,14 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "matplotlib",
+#     "numpy",
+#     "pandas",
+#     "seaborn",
+#     "requests",
+# ]
+# ///
+
 import os
 import sys
 import pandas as pd
